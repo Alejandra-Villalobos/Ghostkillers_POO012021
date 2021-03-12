e569c3b085fdcc76d0fdab00a169c3d14839665c
Alejandra-Villalobos <00132820@uca.edu.sv>
2021-03-11 22:40:44 -0600

    Add: Sum program

---
+++ Ghostkillers_POO012021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ghostkillers_POO012021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a= 0, b= 0;
+
+            Console.WriteLine("Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The sum of {a} + {b} equals {SumNumbers(a, b)}");
+            
+        }
+        public static int SumNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
